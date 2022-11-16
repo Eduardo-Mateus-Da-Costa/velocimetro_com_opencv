db469bbb450db3480742664a75513c024fcba1f0
Eduardo Mateus Da Costa <dudu.costatx11@gmail.com>
2022-11-15 21:06:04 -0300

    Colocando o projeto em um repositório

---
+++ medidor_de_distancia.py
@@ -0,0 +1,65 @@
+import cv2
+
+# TODAS AS MEDIDAS EM CENTÍMETROS
+
+imagem_de_referencia = cv2.imread("imagem_de_referencia.jpg")
+# distancia de referência camera -> objeto
+distancia_de_referencia = 50
+# tamanho (largura) real do objeto
+largura_de_referencia = 17
+
+verde = (0, 255, 0)
+vermelho = (0, 0, 255)
+
+cascade_do_objeto = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+
+def calcular_foco(distancia, largura_na_imagem_de_referencia):
+    foco = (largura_na_imagem_de_referencia * distancia) / largura_de_referencia
+    return foco
+
+
+def calcula_distancia(foco, largura_no_video):
+    distancia = (largura_de_referencia * foco) / largura_no_video
+    return distancia
+
+
+def procura_objeto(image):
+    largura_do_objeto = 0
+    imagem_cinza = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    objetos = cascade_do_objeto.detectMultiScale(imagem_cinza, 1.3, 5)
+    for (x, y, h, w) in objetos:
+        cv2.rectangle(image, (x, y), (x + w, y + h), branco, 1)
+        largura_do_objeto = w
+
+    return largura_do_objeto
+
+
+largura_da_imagem_de_referencia = procura_objeto(imagem_de_referencia)
+foco_referencia = calcular_foco(distancia_de_referencia, largura_da_imagem_de_referencia)
+
+if __name__ == "__main__":
+    video = cv2.VideoCapture("video_teste.mp4")
+    print(foco_referencia)
+    cv2.imshow("ref_image", imagem_de_referencia)
+    print(imagem_de_referencia.shape)
+    foi = False
+    while True:
+        ret, frame = video.read()
+        if ret:
+            largura_do_objeto_no_frame = procura_objeto(frame)
+            if largura_do_objeto_no_frame != 0:
+                distance = calcula_distancia(foco_referencia, largura_do_objeto_no_frame)
+                cv2.putText(
+                    frame, f"Distancia = {round(distance, 2)} CM", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (vermelho), 2
+                )
+                if not foi:
+                    print(frame.shape)
+                    foi = True
+            cv2.imshow("frame", frame)
+        else:
+            break
+        if cv2.waitKey(1) == ord("q"):
+            break
+    video.release()
+    cv2.destroyAllWindows()
---
+++ speed.py
@@ -0,0 +1,74 @@
+import cv2
+import time
+import medidor_de_distancia as md
+
+tempo_inicial = 0
+distancia_inicial = 0
+tempo_corrido = 0
+distancia_corrida = 0
+
+lista_de_distancias = []
+lista_de_velocidades = []
+
+video = cv2.VideoCapture("video_teste.mp4")
+
+
+def calcula_velocidade(distancia, tempo):
+    velocidade = distancia / tempo
+    return velocidade
+
+
+def calcula_media(lista, tamanho):
+    itens = len(lista) - tamanho
+    if itens < 0:
+        itens = len(lista)
+    itens_selecionados = lista[itens:]
+    if len(itens_selecionados) == 0:
+        return 0
+    media = sum(itens_selecionados) / len(itens_selecionados)
+    return media
+
+
+while True:
+    ret, frame = video.read()
+
+    if ret:
+        largura_do_objeto_no_frame = md.procura_objeto(frame)
+        if largura_do_objeto_no_frame != 0:
+            distancia = md.calcula_distancia(md.foco_referencia, largura_do_objeto_no_frame)
+            lista_de_distancias.append(distancia)
+            distancia_media = calcula_media(lista_de_distancias, 2)
+
+            distancia_em_metros = distancia / 100
+
+            if distancia_inicial != 0:
+                distancia_corrida = distancia_inicial - distancia_em_metros
+
+                if distancia_corrida < 0:
+                    distancia_corrida * -1
+
+                tempo_corrido = time.time() - tempo_inicial
+
+                velocidade = calcula_velocidade(distancia_corrida, tempo_corrido)
+                lista_de_velocidades.append(velocidade)
+                media_velocidade = calcula_media(lista_de_velocidades, 2)
+                if media_velocidade < 0:
+                    media_velocidade = media_velocidade * -1
+
+                cv2.putText(
+                    frame, f"Velocidade: {round(media_velocidade, 2)} m/s", (50, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.6,
+                    md.vermelho, 2)
+
+            distancia_inicial = distancia_em_metros
+            tempo_inicial = time.time()
+
+            cv2.putText(
+                frame, f"Distancia = {round(distancia_em_metros, 2)} m", (50, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, md.verde, 2)
+        # recording the video
+        cv2.imshow("Video", frame)
+    else:
+        break
+    if cv2.waitKey(1) == ord("q"):
+        break
+video.release()
+cv2.destroyAllWindows()
